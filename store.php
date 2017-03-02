a061422e4e1383f01bdb1f1800c2be3dfc2390f3
amit <amw.amitjain@gmail.com>
2017-03-02 15:58:06 +0530

    first commit for test from amit.

---
+++ store.php
@@ -0,0 +1,32 @@
+<html>
+    <title>student Info</title>
+    <body>
+        <center>
+        <form method="post">
+            <table border="3" bgcolor="pink" cellspacing="10" cellpadding="5">
+                <tr>
+                    <td><b>Name</b></td>
+                    <td><input type="text" name="name" ></td>
+                </tr>
+                <tr>
+                        <td><b>Math</b></td>
+                        <td><input type="text" name="math" ></td>
+                </tr>
+                <tr>
+                    <td><b>Chemistry</b></td>
+                    <td><input type="text" name="chemistry" ></td>
+                </tr>
+                <tr>
+                    <td><b>Physics</b></td>
+                    <td><input type="text" name="physics" ></td>
+                </tr>
+                    <tr>
+                        <td><b>English</b></td>
+                    <td><input type="text" name="english" ></td>
+                </tr>
+            </table>
+            </form>
+        </center>
+        
+    </body>
+</html>
